4081595603a904793eb923d551014d89db5f47d9
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-03-16 15:04:58 +0800

    ok: 2x speed up

--- day3002-front-matter/main.js
+++ day3002-front-matter/main.js
@@ -11,36 +11,37 @@ if (args.length >= 1) {
   const dir = _d ?? ".";
   const RGX_PATTERN = new RegExp(rgx);
 
-  const interestedFiles = (await fs.readdir(dir))
+  const prependAllMatchedFiles = (await fs.readdir(dir))
     .filter((f) => RGX_PATTERN.test(f))
-    .map((f) => path.join(dir, f));
+    .map((file) =>
+      (async (f) => {
+        let isFirstChunk = true;
+        const prependFrontMatter = async function* (source) {
+          for await (const chunk of source) {
+            const isStartWithFrontMatter =
+              0 ===
+              chunk
+                .slice(0, FRONT_MATTER_ENCODED.length)
+                .compare(FRONT_MATTER_ENCODED);
+            if (isFirstChunk && !isStartWithFrontMatter) {
+              yield FRONT_MATTER_ENCODED;
+              isFirstChunk = false;
+            }
+            yield chunk;
+          }
+        };
 
-  for (const f of interestedFiles) {
-    let isFirstChunk = true;
-    const prependFrontMatter = async function* (source) {
-      for await (const chunk of source) {
-        const isStartWithFrontMatter =
-          0 ===
-          chunk
-            .slice(0, FRONT_MATTER_ENCODED.length)
-            .compare(FRONT_MATTER_ENCODED);
-        if (isFirstChunk && !isStartWithFrontMatter) {
-          yield FRONT_MATTER_ENCODED;
-          isFirstChunk = false;
-        }
-        yield chunk;
-      }
-    };
-
-    const after = f + "-lorem";
-    await pipeline(
-      createReadStream(f),
-      prependFrontMatter,
-      createWriteStream(after),
+        const after = f + "-lorem";
+        await pipeline(
+          createReadStream(f),
+          prependFrontMatter,
+          createWriteStream(after),
+        );
+        await fs.rm(f);
+        await fs.rename(after, f);
+      })(path.join(dir, file)),
     );
-    await fs.rm(f);
-    await fs.rename(after, f);
-  }
+  await Promise.all(prependAllMatchedFiles);
 } else {
   console.info(
     `prepend lorem ipsum to files in DIR that match REGEX_PATTERN
