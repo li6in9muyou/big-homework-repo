aa2241e6e3c94ac4c8939aeeee7e4485adfa0d7c
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-03-23 11:31:01 +0800

    ok: accept only /upload

--- day3005-upload-download/index.js
+++ day3005-upload-download/index.js
@@ -4,34 +4,37 @@ const path = require("path");
 const mp = require("multiparty");
 
 const server = http.createServer((req, res) => {
+  if (req.url !== "/upload") {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
   switch (req.method) {
     case "OPTION":
       break;
     case "POST":
-      if (req.url === "/upload") {
-        const form = new mp.Form();
-        let thisFile = null;
+      const form = new mp.Form();
+      let thisFile = null;
 
-        form.on("error", (e) => {
-          res.statusCode = 500;
-          res.end(`error: ${JSON.stringify(e)}`);
-        });
-        form.on("part", (part) => {
-          if (!part.filename) {
-            return;
-          }
+      form.on("error", (e) => {
+        res.statusCode = 500;
+        res.end(`error: ${JSON.stringify(e)}`);
+      });
+      form.on("part", (part) => {
+        if (!part.filename) {
+          return;
+        }
 
-          thisFile = path.join(__dirname, "upload", part.filename);
-          const writeStream = fs.createWriteStream(thisFile);
-          part.pipe(writeStream);
-        });
+        thisFile = path.join(__dirname, "upload", part.filename);
+        const writeStream = fs.createWriteStream(thisFile);
+        part.pipe(writeStream);
+      });
 
-        form.on("close", () => {
-          res.end(thisFile ?? "bad path");
-        });
+      form.on("close", () => {
+        res.end(thisFile ?? "bad path");
+      });
 
-        form.parse(req);
-      }
+      form.parse(req);
       break;
     default:
       res.statusCode = 400;
